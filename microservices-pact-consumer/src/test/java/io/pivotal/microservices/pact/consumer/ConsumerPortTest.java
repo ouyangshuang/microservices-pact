359800e396ad92e5cfeb9d643ade60e13c16c09a
Pierre-Etienne Poirot <pepoirot@gmx.com>
2016-07-14 23:17:22 +1000

    Upgrade Pact to 3.2.9.

--- microservices-pact-consumer/src/test/java/io/pivotal/microservices/pact/consumer/ConsumerPortTest.java
+++ microservices-pact-consumer/src/test/java/io/pivotal/microservices/pact/consumer/ConsumerPortTest.java
@@ -1,6 +1,9 @@
 package io.pivotal.microservices.pact.consumer;
 
-import au.com.dius.pact.consumer.*;
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRule;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.model.PactFragment;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,25 +17,26 @@ import static junit.framework.TestCase.assertEquals;
 public class ConsumerPortTest {
 
     @Rule
-    public PactRule rule = new PactRule("localhost", 8080, this);
+    public PactProviderRule rule = new PactProviderRule("Foo_Provider", "localhost", 8080, this);
 
-    @Pact(state="Foo_State", provider="Foo_Provider", consumer="Foo_Consumer")
-    public PactFragment createFragment(ConsumerPactBuilder.PactDslWithProvider.PactDslWithState builder) {
+    @Pact(consumer="Foo_Consumer")
+    public PactFragment createFragment(PactDslWithProvider builder) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json;charset=UTF-8");
 
-        return builder.uponReceiving("a request for Foos")
-                .path("/foos")
-                .method("GET")
-
+        return builder
+                .given("Foo_State")
+                .uponReceiving("a request for Foos")
+                    .path("/foos")
+                    .method("GET")
                 .willRespondWith()
-                .headers(headers)
-                .status(200)
-                .body("[{\"value\":42}, {\"value\":100}]").toFragment();
+                    .headers(headers)
+                    .status(200)
+                    .body("[{\"value\":42}, {\"value\":100}]").toFragment();
     }
 
     @Test
-    @PactVerification("Foo_State")
+    @PactVerification("Foo_Provider")
     public void runTest() {
         assertEquals(new ConsumerPort("http://localhost:8080").foos(), Arrays.asList(new Foo(42), new Foo(100)));
     }
